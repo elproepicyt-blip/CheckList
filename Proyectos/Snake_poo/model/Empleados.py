e33c35e712a36931de7e049d42cea123c46b199c
elproepicyt-blip <elproepicyt@gmail.com>
2025-11-26 15:30:34 -0300

    Delete Proyectos/Snake_poo directory

--- Proyectos/Snake_poo/model/Empleados.py
+++
@@ -1,8 +0,0 @@
-class Empleados:
-    def __init__(self,ID,Nombre,Apellido):
-        self.id = ID
-        self.nombre = Nombre
-        self.apellido = Apellido
-
-
-        
--- Proyectos/Snake_poo/model/Horarios.py
+++
@@ -1,5 +0,0 @@
-class Horarios:
-    def __init__(self,Empleado_Id,HorarioEntrada=None,HorarioSalida=None):
-        self.EmpleadoID = Empleado_Id
-        self.H_E = HorarioEntrada
-        self.H_S = HorarioSalida
